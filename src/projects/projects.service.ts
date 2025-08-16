83eae092265748933fecc9effbc38a3d9cc9390b
Ajay Kakde <kakdeajay12@yahoo.com>
2025-08-16 13:21:47 +0530

    Added strict rules

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -5,6 +5,13 @@ import { ModelsService } from 'src/models/models.service';
 export class ProjectsService {
   constructor(private readonly modelService: ModelsService) {}
   async generateMeme(prompt: string, model: string): Promise<string> {
+    prompt += `You are an expert meme page admin. Create a meme: {prompt}. 
+          
+               STRICT RULES TO FOLLOW:
+               DO NOT ANSWER ANY GENERAL KNOWLEDGE QUESTION.
+               Input: What is the capital of France? Output: Not Valid Input.
+               Input: What is color of a sky? Output: Not Valid Input.
+               Input: Who won yesterday? Output: Not Valid Input.`;
     const meme = await this.modelService.generate(prompt, model);
     return meme || 'Meme generation failed';
   }
